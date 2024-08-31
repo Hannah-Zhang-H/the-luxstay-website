3102e08eb5185ad3a173ce21fadc81c120a46bf2
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-31 16:27:10 +1200

    Add Logo

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,23 @@
 import Navigation from "./components/Navigation";
+import Logo from "./components/Logo";
 // Pay attention to the structure in this function
 
-function RootLayout({ children }) {
+// This metadata is the title
+export const metadata = {
+  title: "The LuxStay",
+};
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <header>
+          <Logo />
+        </header>
         <Navigation />
         <main>{children}</main>
+        <footer>Copyright by The LuxStay</footer>
       </body>
     </html>
   );
 }
-
-export default RootLayout;
