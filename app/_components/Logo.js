b59c4344b919134361a3b989225fa729cf377337
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-01 18:13:57 +1200

    Build the Home Page

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-5 z-10">
-      <Image src="/icon.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        alt="The LuxStay logo"
+        quality={100}
+      />
       <span className="text-xl font-semibold text-primary-100">
         The LuxStay
       </span>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
-import Navigation from "./_components/Navigation";
+import Image from "next/image";
+// I don't want to specify the actual width and height, so I import this image
+import bg from "@/public/about-1.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <h1>The lux-stay. Welcome to paradise</h1>
-      {/* use Link instead of a, the page will ne refetched again. Speed up. */}
-      <Link href="/villas">Explore luxtury villas</Link>
-    </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        alt="Mountains and forests with two cabins"
+        fill
+        className="object-cover object-top"
+        placeholder="blur"
+        quality={80}
+      />
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-900 mb-8  tracking-tight font-normal">
+          Welcome home.
+        </h1>
+        <Link
+          href="/villas"
+          className="bg-primary-800 px-8 py-6 text-accent-50 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury villas
+        </Link>
+      </div>
+    </main>
   );
 }
