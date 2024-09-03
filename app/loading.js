871314aef59067131b66129a05b11defd00798bf
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-03 19:21:17 +1200

    Add specific loading.js file in villa file

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,7 @@
 // This is a global loading file, will apply to any page that is in the application
 
+import Spinner from "@/app/_components/Spinner";
+
 export default function Loading() {
-  return <p>LOADING DATA.....</p>;
+  return <Spinner />;
 }
