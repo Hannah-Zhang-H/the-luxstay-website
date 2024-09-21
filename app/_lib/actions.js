a2c12a40b1d944ba35c8cfc560d6db30b782aca3
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-21 17:54:15 +1200

    Delete a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,7 +4,9 @@ import { getServerSession } from "next-auth/next";
 import { authConfig } from "@/app/api/auth/[...nextauth]/route";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
+//======================= updateGuestInfo ==========================
 export async function updateGuestInfo(formData) {
   const session = await getServerSession(authConfig);
   if (!session) throw new Error("You must be logged in");
@@ -28,4 +30,24 @@ export async function updateGuestInfo(formData) {
 
   // Clean all the cache in this route and reload the data as long as the flag get changed
   revalidatePath("/account/profile");
+}
+
+//======================= deleteReservation ==========================
+export async function deleteReservation(bookingId) {
+  const session = await getServerSession(authConfig);
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBooingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBooingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
 }
